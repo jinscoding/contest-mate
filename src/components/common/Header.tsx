397d4447cf0f28b399013060f6b629202b067b53
shgusgh12 <https://github.com/shgusgh12>
2024-07-19 02:30:19 +0900

    [refactor] : 헤더 디자인 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className='w-full mx-auto p-2'>
-      <div className='container mx-auto flex justify-between items-center'>
-        <h2 className='font-bold text-orange-500'>Contest-Mate</h2>
+    <header className="w-full p-2 mx-auto">
+      <div className="container flex items-center justify-between mx-auto">
+        <h2 className="font-bold text-orange-500">Contest-Mate</h2>
         <nav>
-          <ul className='flex gap-4'>
+          <ul className="flex gap-4">
             <li>
-              <Link to='/'>홈</Link>
+              <Link to="/">홈</Link>
             </li>
             <li>
-              <Link to='/login'>로그인</Link>
+              <Link to="/login">로그인</Link>
             </li>
           </ul>
         </nav>

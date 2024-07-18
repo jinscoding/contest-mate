0b571c4afaebce29ee79ddb0fc4ca49d0315fe6c
shgusgh12 <https://github.com/shgusgh12>
2024-07-19 02:28:40 +0900

    [add] : 주요 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#F4F7F8",
+        primary: "#F97316",
+        kakao: "#F8E71C",
+        naver: "#2DB400",
+      },
+    },
   },
   plugins: [],
 };
